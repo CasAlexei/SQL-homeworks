e5cab0aae95d59a9c860a6068f98768e68a45497
CasAlexei <hp81@mail.ru>
2021-04-14 00:08:09 +0300

    change files to migrate. they are not work yet.

--- src/main/resources/db/migration/V20191004052300__create_ref_constraints.sql
+++ src/main/resources/db/migration/V20191004052300__create_ref_constraints.sql
@@ -6,6 +6,3 @@ ALTER TABLE PAY
     ADD CONSTRAINT LOC_C_ID_FK FOREIGN KEY (COUNTRY_ID)
         REFERENCES COUNTRIES (COUNTRY_ID);
 
-ALTER TABLE FOREIGN KEY (pay_id) REFERENCES pay(pay_id),
-FOREIGN KEY (project_id) REFERENCES projects(project_id),
-UNIQUE (pay_id, project_id);
