ed0a7a9b63fd666a92a33f9f64f0cc2e111f9eec
CasAlexei <hp81@mail.ru>
2021-04-14 00:06:27 +0300

    change files to migrate

---
+++ src/main/resources/db/migration/V20191004052100__create_relation_table .sql
@@ -0,0 +1,13 @@
+CREATE TABLE ProjectsEmployeesRelation
+(
+    project_id        NUMBER(6)       NOT NULL,
+    employee_id       NUMBER(6)       NOT NULL,
+
+    FOREIGN KEY (employee_id) REFERENCES employees(employee_id),
+    FOREIGN KEY (project_id) REFERENCES projects(project_id),
+    UNIQUE (employee_id, project_id);
+);
+
+COMMENT ON TABLE projects IS 'Relation table many-to-many. References with employees table and projects table.';
+COMMENT ON COLUMN project_id IS 'Primary key of projects table.';
+COMMENT ON COLUMN employee_id IS 'Primary key of employees table';
