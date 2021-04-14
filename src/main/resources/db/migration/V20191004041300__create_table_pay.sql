aa7fd83efa9cf146b2e386e0a29914588f60c734
CasAlexei <hp81@mail.ru>
2021-04-14 11:13:47 +0300

    test OK

--- src/main/resources/db/migration/V20191004041300__create_table_pay.sql
+++ src/main/resources/db/migration/V20191004041300__create_table_pay.sql
@@ -1,10 +1,9 @@
 CREATE TABLE pay
 (
      cardNr             NUMBER(6)       PRIMARY KEY,
-     employee_id        NUMBER(6),
+     employee_id        NUMBER(6)       UNIQUE NOT NULL,
 
-
-     CONSTRAINT FK_EMPLOYEE_ID UNIQUE FOREIGN KEY (employee_id) REFERENCES employees(employee_id)
+     CONSTRAINT FK_EMPLOYEE_ID FOREIGN KEY (employee_id) REFERENCES employees(employee_id)
 );
 
 CREATE SEQUENCE pay_seq NOCACHE;
@@ -12,8 +11,8 @@ CREATE SEQUENCE pay_seq NOCACHE;
 ALTER TABLE pay
     MODIFY cardNr DEFAULT pay_seq.nextval;
 
-COMMENT ON TABLE pay IS 'Pay table. References with employees table.';
-COMMENT ON COLUMN pay.cardNr IS 'Primary key of pay table.';
-COMMENT ON COLUMN pay.pay_id IS 'Unique foreign key to employees table.';
-COMMENT ON COLUMN pay.salary IS 'Foreign key to employees table.';
-COMMENT ON COLUMN pay.commission_pct IS 'Foreign key to employees table.';
+--COMMENT ON TABLE pay IS 'Pay table. References with employees table.';
+--COMMENT ON COLUMN pay.cardNr IS 'Primary key of pay table.';
+--COMMENT ON COLUMN pay.pay_id IS 'Unique foreign key to employees table.';
+--COMMENT ON COLUMN pay.salary IS 'Foreign key to employees table.';
+--COMMENT ON COLUMN pay.commission_pct IS 'Foreign key to employees table.';
--- src/main/resources/db/migration/V20191004042100__create_ref_constraints.sql
+++
@@ -1,8 +0,0 @@
---------------------------------------------------------
---  Ref Constraints for Table PAY
---------------------------------------------------------
-
-ALTER TABLE PAY
-    ADD CONSTRAINT FK_PAY FOREIGN KEY (PAY_ID)
-        REFERENCES  EMPLOYEES (EMPLOYEE_ID);
-
